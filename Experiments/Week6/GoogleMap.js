0797f12853da1484173f87d70baa0c414ec27675
Ankit <aankit@ccs.neu.edu>
2015-03-01 10:38:06 -0500

    Week 6
    Week 6

---
+++ Experiments/Week6/GoogleMap.js
@@ -0,0 +1,19 @@
+﻿
+      var myCenter = new google.maps.LatLng(42.342, -71.12085);
+function initialize() {
+    var mapOptions = {
+        center: { lat: 42.347, lng: -71.124 },
+        zoom: 10
+    };
+         
+    var map = new google.maps.Map(document.getElementById('map-api'),
+        mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myCenter,
+
+    });
+
+    marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ Experiments/Week6/Routing.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("Demo", ['ngRoute']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: '/home.html',
+        }).
+        when('/about', {
+            templateUrl: '/about.htm',
+        }).
+        when('/contact', {
+            templateUrl: '/contact.htm',
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
