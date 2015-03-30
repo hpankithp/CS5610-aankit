05380f1c94ba57a0161efc92e6fcaf6d872f9069
Ankit <aankit@ccs.neu.edu>
2015-03-30 00:11:39 -0400

    Week 9 Project Experimentation

---
+++ Experiments/Week9/JSExperiment.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $("button").click(function () {
+        $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
+    });
+
+
+    $("#btn1").click(function () {
+        $("p").append(" <b>Anksss</b>.");
+    });
+});
